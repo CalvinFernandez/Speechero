c31380345eee699f90ed05f720da284051a8e8e3
Deniz Kahramaner <dennizk@gmail.com>
2013-03-06 18:48:48 -0800

    Updated the testing folder with the latest code in the master branch. Going to add the same folder to the prototype3 branch soon

--- testing/progressHero.js
+++ testing/progressHero.js
@@ -8,6 +8,25 @@ var progressHero =
 		var curWidth = 0;
 		window.setInterval(function(){
 			curWidth += 100*intervalTime/totalTime;
+			switch(Math.floor(curWidth / 25))
+			{
+				case 0:
+					$("#intro").html("<h1>Introduction</h1>");
+					break;
+				case 1:
+					$("#intro").html("<h3>Introduction</h3>");
+					$("#arg1").html("<h1>Argument 1</h1>");
+					break;
+				case 2:
+					$("#arg1").html("<h3>Argument 1</h3>");
+					$("#arg2").html("<h1>Argument 2</h1>");
+					break;
+				case 3:
+					$("#arg2").html("<h3>Argument 2</h3>");
+					$("#conclusion").html("<h1> Conclusion </h1>")
+				default:
+					break;
+			}
 			$(".bar").css("width",curWidth.toString()+"%");
 		},intervalTime*1000);		
 	}
--- testing/waves.js
+++ testing/waves.js
@@ -1,18 +1,18 @@
 var wave = function()
 {
 	var self = this;
-	var QUALITY = 8;
-	var WIDTH = Math.floor( window.innerWidth / QUALITY );
-	var HEIGHT = Math.floor( window.innerHeight / QUALITY );
-	var CANVAS_WIDTH = window.innerWidth;
-	var CANVAS_HEIGHT = window.innerHeight,
+	var QUALITY = 1;
+	var WIDTH = 300 / QUALITY;//Math.floor( window.innerWidth / QUALITY );
+	var HEIGHT = 375 / QUALITY;//Math.floor( window.innerHeight / QUALITY );
+	var CANVAS_WIDTH = 300;//window.innerWidth;
+	var CANVAS_HEIGHT = 375,//window.innerHeight,
         SIZE = WIDTH*HEIGHT,
         context, image, data,
         buffer1, buffer2, 
         tempbuffer, pointers, 
         userInteraction;
 
-	this.init = function(id, parentID)
+	this.init = function(parentID, id)
 	{
 		self._id = "waveid";
 		self._class = "wave";
@@ -43,12 +43,13 @@ var wave = function()
 			self._parent = document.createElement("div");
 			self._parent.setAttribute("id", self._parentID);
 			self._parent.setAttribute("class", self._class);
+			document.body.appendChild(self._parent);
 		}
-		document.body.appendChild(self._parent);
+
 		self._parent.appendChild(self._canvas);
 
 		context = self._canvas.getContext("2d");
-		context.fillStyle = "rgb(99,219,255)";
+		context.fillStyle = "rgb(20,155,223)";
 		context.fillRect(0, 0, WIDTH, HEIGHT);
 		image = context.getImageData(0, 0, WIDTH, HEIGHT);
 		data = image.data;
@@ -68,24 +69,41 @@ var wave = function()
 	{
 		userInteraction = true;
 		var hipCenter = coords[0][heroKinectEngine.jnt("HIP_CENTER")];
-		var handLeft = coords[0][heroKinectEngine.jnt("HAND_LEFT")];
-		var handRight = coords[0][heroKinectEngine.jnt("HAND_RIGHT")];
+		//var handLeft = coords[0][heroKinectEngine.jnt("HAND_LEFT")];
+		//var handRight = coords[0][heroKinectEngine.jnt("HAND_RIGHT")];
 
-		handRight.x = heroKinectEngine.fixZeroTo200(handRight.x);
-		handRight.y = heroKinectEngine.fixZeroTo200(handRight.y);
+		//handRight.x = heroKinectEngine.fixZeroTo200(handRight.x);
+		//handRight.y = heroKinectEngine.fixZeroTo200(handRight.y);
 
-		handLeft.x = heroKinectEngine.fixZeroTo200(handLeft.x);
-		handLeft.y = heroKinectEngine.fixZeroTo200(handLeft.y);
+		//handLeft.x = heroKinectEngine.fixZeroTo200(handLeft.x);
+		//handLeft.y = heroKinectEngine.fixZeroTo200(handLeft.y);
 
 		//hipCenter.x = heroKinectEngine.fixZeroTo200(hipCenter.x);
 		//hipCenter.y = heroKinectEngine.fixZeroTo200(hipCenter.y);
 
 		pointers = [[( ( hipCenter.x / 100 ) * CANVAS_WIDTH ) / QUALITY, 
 					 ( ( hipCenter.y / 100 ) * CANVAS_HEIGHT ) / QUALITY],
-					[( ( handLeft.x / 200 ) * CANVAS_WIDTH ) / QUALITY ,
-					 ( ( handLeft.y / 200 ) * CANVAS_HEIGHT )/ QUALITY ],
-					[( ( handRight.x / 200) * CANVAS_WIDTH ) / QUALITY ,
-					 ( ( handRight.y / 200) * CANVAS_HEIGHT) / QUALITY ]];
+					 [( ( (hipCenter.x + 1) / 100 ) * CANVAS_WIDTH ) / QUALITY, 
+					 ( ( (hipCenter.y + 1) / 100 ) * CANVAS_HEIGHT ) / QUALITY],
+					 [( ( (hipCenter.x - 1) / 100 ) * CANVAS_WIDTH ) / QUALITY, 
+					 ( ( (hipCenter.y - 1) / 100 ) * CANVAS_HEIGHT ) / QUALITY],
+					 [( ( (hipCenter.x + 1) / 100 ) * CANVAS_WIDTH ) / QUALITY, 
+					 ( ( (hipCenter.y - 1) / 100 ) * CANVAS_HEIGHT ) / QUALITY],
+					 [( ( (hipCenter.x - 1) / 100 ) * CANVAS_WIDTH ) / QUALITY, 
+					 ( ( (hipCenter.y + 1) / 100 ) * CANVAS_HEIGHT ) / QUALITY],
+					 [( ( (hipCenter.x + 2) / 100 ) * CANVAS_WIDTH ) / QUALITY, 
+					 ( ( (hipCenter.y + 2) / 100 ) * CANVAS_HEIGHT ) / QUALITY],
+					 [( ( (hipCenter.x - 2) / 100 ) * CANVAS_WIDTH ) / QUALITY, 
+					 ( ( (hipCenter.y - 2) / 100 ) * CANVAS_HEIGHT ) / QUALITY],
+					 [( ( (hipCenter.x + 2) / 100 ) * CANVAS_WIDTH ) / QUALITY, 
+					 ( ( (hipCenter.y - 2) / 100 ) * CANVAS_HEIGHT ) / QUALITY],
+					 [( ( (hipCenter.x - 2) / 100 ) * CANVAS_WIDTH ) / QUALITY, 
+					 ( ( (hipCenter.y + 2) / 100 ) * CANVAS_HEIGHT ) / QUALITY]];
+
+					//[( ( handLeft.x / 200 ) * CANVAS_WIDTH ) / QUALITY ,
+					 //( ( handLeft.y / 200 ) * CANVAS_HEIGHT )/ QUALITY ],
+					//[( ( handRight.x / 200) * CANVAS_WIDTH ) / QUALITY ,
+					 //( ( handRight.y / 200) * CANVAS_HEIGHT) / QUALITY ]];
 	}
 
 	this.setWidth = function(width)
@@ -132,7 +150,7 @@ var wave = function()
 
 			data[ (i * 4) + 0 ] = pixel;
 			//data[ (i + 4) + 1] = pixel < 219 ? 219 : pixel;
-			data[ ((i + 1) * 4) + 2 ] = 255;
+			data[ ((i + 1) * 4) + 2 ] = 223;
 
 			//data[ (i * 4) + 0 ] = pixel*2;
 			//data[ ((i + 1) * 4) + 2 ] = pixel*0.3;
